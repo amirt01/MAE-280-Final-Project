0474658c52b14e3c9d28563a1e6a8173e05954d0
amir tadros <amirtadros01@gmail.com>
2023-02-15 10:21:08 -0800

    Motor Array
    Vector : 3 doubles
    Motor : 2 vectors (position, magnitude)
    Motor Motors[N_MOTORS]

---
+++ main/main.ino
@@ -0,0 +1,37 @@
+struct Vector {
+  double x, y, z;
+
+  Vector(const double&_x, const double&_y, const double&_z)
+    : x(_x), y(_y), z(_z) {}
+};
+
+struct Motor {
+  Vector position;
+  Vector magnitude;
+
+  Motor(const double& pos_x = 0, const double& pos_y = 0, const double& poz_z = 0,
+        const double& mag_x = 0, const double& mag_y = 0, const double& mag_z = 0)
+    : position(pos_x, pos_y, poz_z), magnitude(mag_x, mag_y, mag_z) {}
+
+  Motor(Vector pos, Vector mag) : position(pos), magnitude(mag) {}
+};
+
+const int N_MOTORS = 6;
+Motor Motors[N_MOTORS] = {
+  {},
+  {},
+  {},
+  {},
+  {},
+  {}
+};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
