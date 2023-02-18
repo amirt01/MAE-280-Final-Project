7776dd3ab5bd6b15302e81eab0de6b45290271a6
amir tadros <amirtadros01@gmail.com>
2023-02-18 10:11:23 -0800

    UpdateMotors and helper functions

--- main/main.ino
+++ main/main.ino
@@ -3,6 +3,10 @@ struct Vector {
 
   Vector(const double& _x, const double& _y, const double& _z)
     : x(_x), y(_y), z(_z) {}
+
+  Vector operator- (const Vector& vect) const {
+    return {x - vect.x, y - vect.y, z - vect.z};
+  }
 };
 
 struct Motor {
@@ -22,13 +26,20 @@ struct Motor {
 
 Vector Moment(const Motor&);
 
+void ReadSensors();  // read accelerometer/gyro and depth
+void UpdateVars();   // calculate and update variables 
+void UpdateMotors(); // write to all the motors
+
 const int N_MOTORS = 4;
-Motor Motors[N_MOTORS] = {
+Motor motors[N_MOTORS] = {
   {},
   {},
   {},
   {}
 };
+
+Vector target_direction(0, 0, 0);
+Vector true_direction(0, 0, 0);
 
 void setup() {
   // put your setup code here, to run once:
@@ -40,8 +51,9 @@ void loop() {
 
   // take in data
   // process data
-  // update motors
+  UpdateMotors();
 
+  for (const Motor& motor : motors) { analogWrite(motor.pin, motor.mag); }
 }
 
 Vector Moment(const Motor& m) {
@@ -49,4 +61,20 @@ Vector Moment(const Motor& m) {
   return {m.pos.y * m.dir.z - m.pos.z * m.dir.y,
           -(m.pos.x * m.dir.z - m.pos.z * m.dir.x),
           m.pos.x * m.dir.y - m.pos.y * m.dir.x};
+}
+
+void UpdateMotors() {
+  Vector accel_direction = target_direction - true_direction;
+  
+  if (accel_direction.x > 0) {}
+  else if (accel_direction.x < 0) {}
+  else {}
+
+  if (accel_direction.y > 0) {}
+  else if (accel_direction.y < 0) {}
+  else {}
+  
+  if (accel_direction.z > 0) {}
+  else if (accel_direction.z < 0) {}
+  else {}
 }
